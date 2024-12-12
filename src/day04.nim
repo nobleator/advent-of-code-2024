4a82cce12094df9bf9121cd6f12f7ba70e0cc023
nobleator <noble.ben.16@gmail.com>
2024-12-11 21:24:17 -0500

    Day 4 part 2

--- src/day04.nim
+++ src/day04.nim
@@ -47,12 +47,30 @@ proc d04a(input: string): int =
     return ctr
 
 proc d04b(input: string): int =
-    return -1
+    var file = readFile(input)
+    var rowCount = file.countLines()
+    var colCount = len(file.split('\n', maxsplit=1)[0])
+    var g: seq[seq[char]] = newSeq[seq[char]](rowCount)
+    var r = 0
+    var row = newSeq[char](colCount)
+    for line in file.splitLines():
+        for c, ch in line:
+            row[c] = ch
+        g[r] = row
+        r += 1
+    var ctr = 0
+    for r, row in g:
+        for c, val in row:
+            if val == 'A' and c > 0 and c < colCount-1 and r > 0 and r < rowCount-1:
+                var corners = g[r-1][c-1] & g[r-1][c+1] & g[r+1][c-1] & g[r+1][c+1]
+                if count(corners, 'S') == 2 and count(corners, 'M') == 2 and g[r-1][c-1] != g[r+1][c+1]:
+                    ctr += 1
+    return ctr
 
 assert d04a("./test/day04a.txt") == 4
 assert d04a("./test/day04b.txt") == 8
 assert d04a("./test/day04c.txt") == 18
 echo "Part 1: ", d04a("./inputs/day04.txt")
 
-# assert d04b("./test/day03b.txt") == 4
-# echo "Part 2: ", d04b("./inputs/day04.txt")
+assert d04b("./test/day04d.txt") == 9
+echo "Part 2: ", d04b("./inputs/day04.txt")
