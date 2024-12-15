43f5050157121588c1433e1a94d12c430304d294
nobleator <noble.ben.16@gmail.com>
2024-12-14 20:27:03 -0500

    Day 7 part 2

--- src/day07.nim
+++ src/day07.nim
@@ -1,6 +1,7 @@
-import std/strutils, std/sequtils, std/sets, std/tables, std/algorithm
+import std/strutils, std/sequtils
 
 const ops = @['+', '*']
+const ops2 = @['+', '*', '|']
 
 func generatePermutations(ops: seq[char], length: int, current: seq[char], result: var seq[seq[char]]) =
     if len(current) == length:
@@ -12,9 +13,9 @@ func generatePermutations(ops: seq[char], length: int, current: seq[char], resul
             generatePermutations(ops, length, newSeq, result)
 
 func getPermutations(ops: seq[char], length: int): seq[seq[char]] =
-    var result: seq[seq[char]] = @[]
-    generatePermutations(ops, length, @[], result)
-    return result
+    var res: seq[seq[char]] = @[]
+    generatePermutations(ops, length, @[], res)
+    return res
 
 proc d07a(input: string): int =
     var text = readFile(input)
@@ -40,10 +41,31 @@ proc d07a(input: string): int =
     overall
 
 proc d07b(input: string): int =
-    -1
+    var text = readFile(input)
+    var overall = 0
+    for line in text.splitLines():
+        var segments = line.split(": ")
+        var lhs = parseInt(segments[0])
+        var rhs = segments[1].split().map(parseInt)
+        for p in getPermutations(ops2, len(rhs)-1):
+            var total = rhs[0]
+            for i in 0..len(rhs)-2:
+                var nv = rhs[i+1]
+                if p[i] == '+':
+                    total += nv
+                elif p[i] == '*':
+                    total *= nv
+                elif p[i] == '|':
+                    total = parseInt($total & $nv)
+                if total > lhs:
+                    break
+            if total == lhs:
+                overall += lhs
+                break
+    overall
 
 assert d07a("./test/day07.txt") == 3749
 echo "Part 1: ", d07a("./inputs/day07.txt")
 
-# assert d07b("./test/day07.txt") == 6
-# echo "Part 2: ", d07b("./inputs/day07.txt")
+assert d07b("./test/day07.txt") == 11387
+echo "Part 2: ", d07b("./inputs/day07.txt")
