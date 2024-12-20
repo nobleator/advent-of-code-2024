d4954bdc8a0111fd56ada42ed2439189ac174ff3
nobleator <noble.ben.16@gmail.com>
2024-12-20 08:28:49 -0500

    Day 10 part 2

--- src/day10.nim
+++ src/day10.nim
@@ -1,4 +1,4 @@
-import std/strutils, std/sequtils, std/sets, std/tables, std/math, std/algorithm
+import std/strutils, std/sets, std/math
 import grid
 
 func climableNeighbors(grid: Grid, point: (int, int)): seq[(int, int)] =
@@ -13,12 +13,26 @@ func climableNeighbors(grid: Grid, point: (int, int)): seq[(int, int)] =
                 res.add(n)
     res
 
+func walk(grid: Grid, point: (int, int)): int =
+    let h = parseInt($get(grid, point))
+    if h == 9:
+        return 1
+    let neighbors = getOrthogonal(grid, point)
+    var res: seq[(int, int)]
+    var score = 0
+    for n in neighbors:
+        let c = get(grid, n)
+        if isDigit(c):
+            let v = parseInt($c)
+            if v-h == 1:
+                score += walk(grid, n)
+    return score
+
 proc d10a(input: string): int64 =
     var file = readFile(input)
     var rowCount = file.countLines()
     var colCount = len(file.split('\n', maxsplit=1)[0])
     var g: seq[seq[char]] = newSeq[seq[char]](rowCount)
-    var antennas = initTable[char, HashSet[(int, int)]]()
     var r = 0
     var row = newSeq[char](colCount)
     var trailheads: seq[(int, int)]
@@ -31,7 +45,6 @@ proc d10a(input: string): int64 =
         r += 1
     var score = 0
     for t in trailheads:
-        let h = parseInt($get(g, t))
         var toVisit: seq[(int, int)]
         var visited = initHashSet[(int, int)]()
         visited.incl(t)
@@ -50,7 +63,24 @@ proc d10a(input: string): int64 =
     score
 
 proc d10b(input: string): int =
-   -1
+    var file = readFile(input)
+    var rowCount = file.countLines()
+    var colCount = len(file.split('\n', maxsplit=1)[0])
+    var g: seq[seq[char]] = newSeq[seq[char]](rowCount)
+    var r = 0
+    var row = newSeq[char](colCount)
+    var trailheads: seq[(int, int)]
+    for line in file.splitLines():
+        for c, ch in line:
+            row[c] = ch
+            if ch == '0':
+                trailheads.add((r, c))
+        g[r] = row
+        r += 1
+    var score = 0
+    for t in trailheads:
+        score += walk(g, t)
+    score
 
 assert d10a("./test/day10a.txt") == 1
 assert d10a("./test/day10b.txt") == 2
@@ -59,5 +89,8 @@ assert d10a("./test/day10d.txt") == 3
 assert d10a("./test/day10e.txt") == 36
 echo "Part 1: ", d10a("./inputs/day10.txt")
 
-# assert d10b("./test/day10a.txt") == 2858
-# echo "Part 2: ", d10b("./inputs/day10.txt")
+assert d10b("./test/day10e.txt") == 81
+assert d10b("./test/day10f.txt") == 3
+assert d10b("./test/day10g.txt") == 13
+assert d10b("./test/day10h.txt") == 227
+echo "Part 2: ", d10b("./inputs/day10.txt")
